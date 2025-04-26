94de5553e9cc7f620836c79af4a7b1de217c0331
23f1001945 <23f1001945@ds.study.iitm.ac.in>
2025-04-26 13:52:27 +0530

    more features

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,16 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/listings')
+def listings():
+    return render_template('listings.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/get-quote', methods=['POST'])
 def get_quote():
     name = request.form.get('name')
     email = request.form.get('email')
     phone = request.form.get('phone')
     requirements = request.form.get('requirements')
-    
-    # Here you would normally save data to database or send an email
     print(f"New Inquiry: {name}, {email}, {phone}, {requirements}")
-
     return render_template('thankyou.html', name=name)
 
 if __name__ == '__main__':
